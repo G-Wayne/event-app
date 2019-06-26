c97782de5ad2bff367d842f04c599b1317c7c69c
G-Wayne <gawayne_lawton@yahoo.com>
2019-06-26 19:14:09 -0400

    updates to db and views

--- runapp.py
+++ runapp.py
@@ -2,8 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
+
+
+#get all configurations
 app.config.from_pyfile('config.py')
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
+
+# need to comment this line before creating db from python shell
+from views import *
 
 if __name__=='__main__':
 	app.run(debug=True)
