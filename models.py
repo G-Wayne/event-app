01f11db63748e1d2aba8533810ef03f0f5618023
TGWhite23 <32618336+TGWhite23@users.noreply.github.com>
2019-06-27 03:25:53 -0500

    Update models.py

--- models.py
+++ models.py
@@ -3,7 +3,8 @@ from runapp import db
 class User(db.Model):
 	id 		  = db.Column(db.Integer,primary_key=True)
 	public_id = db.Column(db.String(50),unique=True)
-	name 	  = db.Column(db.String(50))
+	fname 	  = db.Column(db.String(50))
+	lname 	  = db.Column(db.String(50))
 	password  = db.Column(db.String(80))
 	admin 	  = db.Column(db.Boolean)
 
